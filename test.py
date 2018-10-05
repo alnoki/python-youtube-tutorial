e0a098837934866d0c83a3ca5dc2a3afda70c96d
alnoki <43892045+alnoki@users.noreply.github.com>
2018-10-05 15:39:40 -0500

    Tests branching

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 import random
 
 print("Sleeping...")
-time.sleep(2)
+time.sleep(1) # Switched in a branch
 print("Awake")
 print(random.randint(0, 10))
 time.sleep(1) # Added via Git web editor
