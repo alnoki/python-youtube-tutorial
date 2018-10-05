bd3baf7842935086eeded9490c6fbfa4ec01789e
alnoki <43892045+alnoki@users.noreply.github.com>
2018-10-05 15:45:44 -0500

    Simulates code edited remotely

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 import random
 
 print("Sleeping...")
-time.sleep(1) # Switched in a branch
+time.sleep(2) # Simulates code edited remotely
 print("Awake")
 print(random.randint(0, 10))
 time.sleep(1) # Added via Git web editor
